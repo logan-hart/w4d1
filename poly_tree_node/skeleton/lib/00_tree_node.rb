dea8c142361082ae5ce1715d049be9cb8aab082c
Logan Hartman <loganchart@gmail.com>
2022-11-28 14:08:44 -0500

    completed initialize

--- poly_tree_node/skeleton/lib/00_tree_node.rb
+++ poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
+    
+    attr_reader :parent, :children, :value
+
+    def initialize(value, parent=nil, children=[])
+        @parent = parent
+        @children = children
+        @value = value
+    end
+
+    def parent=(node)
+        if !self.parent.nil?
+            self.parent = node
+        end
+        
+    end
 
 end
