16c2b4a07d503581611242626debe1bea22f664a
Logan Hartman <loganchart@gmail.com>
2022-11-28 14:49:58 -0500

    parent= method

--- poly_tree_node/skeleton/lib/00_tree_node.rb
+++ poly_tree_node/skeleton/lib/00_tree_node.rb
@@ -9,15 +9,20 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        if !self.parent.nil?
-            self.parent = node 
-        else
-            self.parent = nil
-        end #This is causing the infinite loop
-            if !self.children.include?(node)
-                node.children << self.children
-            end 
-        # end
+        @parent = node
+        if !@parent.children.include?(self) && !self.children.nil?
+            @parent.children << self
+        end
+        
+        # if !self.parent.nil?
+        #     self.parent = node 
+        # else
+        #     self.parent = nil
+        # end #This is causing the infinite loop
+        #     if !self.children.include?(node)
+        #         node.children << self.children
+        # #     end 
+        # # end
         
     end
 
